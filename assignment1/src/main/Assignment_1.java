18e90135f2226201138e0e91c1ff7e341cc1da1f
sorash <ashrafisoroush@gmail.com>
2016-09-27 11:46:02 -0400

    added square

--- assignment1/src/main/Assignment_1.java
+++ assignment1/src/main/Assignment_1.java
@@ -2,13 +2,24 @@ package main;
 
 public class Assignment_1 
 {
-	private static Circle c = new Circle();
+	// create the shapes
+	private static Circle c;
+	private static Square s;
 
 	public static void main(String[] args)
 	{
 		// set a radius for the circle and show it and the perimeter
+		c = new Circle();
 		c.setRadius(10.5);
 		System.out.println("Circle's radius: " + c.getRadius());
 		System.out.println("Circle's perimeter: " + c.getPerimeter());
+		
+		System.out.println();
+		
+		// set a length for the square and show it and the perimeter
+		s = new Square();
+		s.setLength(25.4);
+		System.out.println("Square's side length: " + s.getLength());
+		System.out.println("Square's perimeter: " + s.getPerimeter());
 	}
 }
---
+++ assignment1/src/main/Square.java
@@ -0,0 +1,37 @@
+package main;
+
+public class Square implements Shape
+{
+	private static double length;
+	
+	/**
+	 * Returns the perimeter of the square.
+	 * 
+	 * @return 4*length to get the perimeter
+	 */
+	@Override
+	public double getPerimeter()
+	{
+		return 4*length;
+	}
+
+	/**
+	 * Gets the length of sides of a square.
+	 * 
+	 * @return length of sides
+	 */
+	public double getLength() 
+	{
+		return length;
+	}
+
+	/**
+	 * Sets the side length value of a square.
+	 * 
+	 * @param length value to set for side lengths
+	 */
+	public void setLength(double length) 
+	{
+		Square.length = length;
+	}
+}
