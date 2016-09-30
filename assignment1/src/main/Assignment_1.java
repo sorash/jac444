d8dc1655a17a0c97288b52dda731d0bd2dfff405
unknown <sashrafi3@SY-T3078-40.SeneDS.senecacollege.ca>
2016-09-30 11:09:41 -0400

    added task 2 and 3 and comments

--- assignment1/src/main/Assignment_1.java
+++ assignment1/src/main/Assignment_1.java
@@ -5,6 +5,8 @@ public class Assignment_1
 	public static void main(String[] args)
 	{
 		int[] values = {2,1,3,5,1,4,5,3,5,7,1,2,8,9};
+		
+		// initialize a list and shapes to add to it
 		ShapeLinkedList sll = new ShapeLinkedList();
 		Circle c1 = new Circle(values[0]);
 		Circle c2 = new Circle(values[1]);
@@ -14,6 +16,8 @@ public class Assignment_1
 		Triangle t2 = new Triangle(values[7],values[8],values[9]);
 		Rectangle r1 = new Rectangle(values[10],values[11]);
 		Rectangle r2 = new Rectangle(values[12],values[13]);
+		
+		// insert shapes into list
 		sll.insertAtBeginning(r1);
 		sll.insertAtBeginning(r2);
 		sll.insertAtBeginning(c1);
@@ -22,6 +26,17 @@ public class Assignment_1
 		sll.insertAtEnd(sq2);
 		sll.insertAtEnd(t1);
 		sll.insertAtEnd(t2);
+		
+		// print list
+		System.out.println(sll.toString());
+		
+		// task 2
+		sll.deleteAtIndex(sll.length - 1);
+		
+		// task 3
+		sll.deleteAtIndex(5);
+		
+		// print list again
 		System.out.println(sll.toString());
 	}
 }
